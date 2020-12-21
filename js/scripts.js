c63d4ce68c119177c3fec0b82b2ff3838a78603b
samuel-7 <71902942+samuel-7@users.noreply.github.com>
2020-12-21 15:04:36 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+(function($) {
+    "use strict";
+
+    // Rolagem suave usando jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 71)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // Selecionar botão do navegar enquanto faz a rolagem
+    $(document).scroll(function() {
+        var scrollDistance = $(this).scrollTop();
+        if (scrollDistance > 100) {
+            $('.scroll-to-top').fadeIn();
+        } else {
+            $('.scroll-to-top').fadeOut();
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    // Adiocionar itens da barra de navegação na rolagem
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 80
+    });
+
+    // recolher a barra de navegação
+    var navbarCollapse = function() {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Recolher se a página não estiver no topo
+    navbarCollapse();
+    // recolher a barra de navegação quando a página for rolada
+    $(window).scroll(navbarCollapse);
+
+})(jQuery);
